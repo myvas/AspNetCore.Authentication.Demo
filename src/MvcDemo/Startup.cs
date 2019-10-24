9d498747d1fedca8a3ebe0e9ec57c2a53338c157
FrankH <4848285@qq.com>
2019-10-24 22:31:17 +0800

    .

--- src/MvcDemo/Startup.cs
+++ src/MvcDemo/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Myvas.AspNetCore.Authentication;
 
 namespace Demo
@@ -87,11 +88,11 @@ namespace Demo
             services.AddViewDivert();
             services.AddAuthorization();
 
-            services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -100,21 +101,23 @@ namespace Demo
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
             }
-
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            //app.UseRouting();
+            app.UseRouting();
             app.UseAuthentication();
-            //app.UseAuthorization();
-            
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
-            app.UseMvcWithDefaultRoute();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
