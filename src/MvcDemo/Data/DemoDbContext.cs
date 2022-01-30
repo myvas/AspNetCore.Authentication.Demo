48e4b76998f5078502f09ba6d83c93fcf2e441a4
FrankH <4848285@qq.com>
2022-01-30 18:49:01 +0800

    untracked files on 3.1: 8e8130f update for Weixin 3.1.200

---
+++ src/MvcDemo/Data/DemoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Myvas.AspNetCore.Weixin;
+
+namespace Demo.Data
+{
+    public class DemoDbContext : IdentityDbContext, IWeixinDbContext<WeixinSubscriber, string>
+    {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WeixinSubscriber> WeixinSubscribers { get; set; }
+        public DbSet<WeixinReceivedEvent> WeixinReceivedEvents { get; set; }
+        public DbSet<WeixinReceivedMessage> WeixinReceivedMessages { get; set; }
+        public DbSet<WeixinResponseMessage> WeixinResponseMessages { get; set; }
+        public DbSet<WeixinSendMessage> WeixinSendMessages { get; set; }
+    }
+}
