a5d72ee3516208db20268c577cb9f2295b5f234d
FrankH <4848285@qq.com>
2025-03-23 21:44:42 +0800

    Update DbContext to IWeixinDbContext 9.0

--- src/MvcDemo/Data/DemoDbContext.cs
+++ src/MvcDemo/Data/DemoDbContext.cs
@@ -1,30 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Myvas.AspNetCore.Weixin;
-using Myvas.AspNetCore.Weixin.EntityFrameworkCore.Options;
-using Myvas.AspNetCore.Weixin.EntityFrameworkCore.Storage.Extensions;
-using Myvas.AspNetCore.Weixin.Models;
-using System;
 
 namespace Demo.Data
 {
-    public class DemoDbContext : IdentityDbContext, IWeixinDbContext<Subscriber>
+    public class DemoDbContext : IdentityDbContext, IWeixinDbContext
     {
-        public DemoDbContext(DbContextOptions<DemoDbContext> options) : base(options)
+        public DemoDbContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<Subscriber> WeixinSubscribers { get; set; }
-        public DbSet<AuditEntry> AuditEntires { get; set; }
-        public DbSet<Subscriber> Subscribers { get; set; }
-        public DbSet<MessageReceivedEntry> MessageReceivedEntries { get; set; }
-        public DbSet<EventReceivedEntry> EventReceivedEntries { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected DemoDbContext()
         {
-            builder.ConfigureWeixinDbContext<Subscriber>(null);
-            base.OnModelCreating(builder);
         }
+
+        public DbSet<WeixinSubscriberEntity> WeixinSubscribers { get; set; }
+        public DbSet<WeixinReceivedEventEntity> WeixinReceivedEvents { get; set; }
+        public DbSet<WeixinReceivedMessageEntity> WeixinReceivedMessages { get; set; }
+        public DbSet<WeixinResponseMessageEntity> WeixinResponseMessages { get; set; }
+        public DbSet<WeixinSendMessageEntity> WeixinSendMessages { get; set; }
     }
 }
